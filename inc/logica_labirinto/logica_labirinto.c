d04082b0aff579cf354a00a23a29e6b4309fa7d4
marcosgabrielms <marcosgabriel166@gmail.com>
2025-09-16 16:45:17 -0300

    :recycle: refactor: refatora a lógica do jogo
    -> Separo a comunicação I2C do Display OLED e MPU6050 que estavam juntas
    -> Modifico a lógica de movimentação do Jogador(LED AZUL)

--- inc/logica_labirinto/logica_labirinto.c
+++ inc/logica_labirinto/logica_labirinto.c
@@ -6,26 +6,45 @@
 #include "inc/mpu6050/mpu6050.h"
 #include <string.h>
 
+// --- INÍCIO DA CORREÇÃO ---
+
+// Pinos I2C corretos para o conector I2C0 da BitDogLab
+#define I2C0_SDA_PIN 0
+#define I2C0_SCL_PIN 1
+
 int main() {
     stdio_init_all();
     npInit(LED_PIN);
     npClear();
     npWrite();
     
+    // Inicializa o display no i2c1
     inicializacao_display(); 
-    mpu6050_init(i2c1);
+
+    // Inicializa o barramento i2c0 para o MPU6050 nos pinos corretos
+    i2c_init(i2c0, 400 * 1000);
+    gpio_set_function(I2C0_SDA_PIN, GPIO_FUNC_I2C);
+    gpio_set_function(I2C0_SCL_PIN, GPIO_FUNC_I2C);
+    gpio_pull_up(I2C0_SDA_PIN);
+    gpio_pull_up(I2C0_SCL_PIN);
+
+    // Inicializa o sensor MPU6050 no i2c0
+    mpu6050_init(i2c0);
     inicializar_jogo();
 
     int16_t accel[3], gyro[3], temp;
-    char Gyro_X_str[20], Gyro_Y_str[20], Direcao_str[20];
+    char Direcao_str[20];
     char Posicao_str[20]; 
+    char Nivel_str[20];
 
     while (true) {
-        mpu6050_read_raw(i2c1, accel, gyro, &temp);
+        mpu6050_read_raw(i2c0, accel, gyro, &temp);
         strcpy(Direcao_str, "Parado");
 
-        int limite_movimento = 10000;
+        // Diminuído o limite para aumentar a sensibilidade do movimento
+        int limite_movimento = 8000; 
 
+        // Lógica de movimento baseada no giroscópio
         if (gyro[1] > limite_movimento) {
             strcpy(Direcao_str, "Para Tras");
             mover_jogador(0, 1);
@@ -42,18 +61,20 @@ int main() {
             mover_jogador(-1, 0);
         }
 
-        sprintf(Gyro_X_str, "Gyro X: %d", gyro[0]);
-        sprintf(Gyro_Y_str, "Gyro Y: %d", gyro[1]);
-        
+        // Prepara as strings para exibição no display
+        sprintf(Nivel_str, "Nivel: %d", nivel);
         sprintf(Posicao_str, "Pos: (%d, %d)", jogador_x, jogador_y);
 
-        display_mensagem("--- DEBUG ---",
-                         Gyro_X_str,
-                         Gyro_Y_str,
+        // Exibe no display as informações do jogo
+        display_mensagem("JOGO LABIRINTO",
+                         Nivel_str,
+                         "",
                          Posicao_str,
-                         "Rotacao:",
-                         Direcao_str,
-                         "", "", true);
+                         "",
+                         "Movimento:",
+                         Direcao_str, 
+                         "", 
+                         true);
 
         desenhar_labirinto();
         verificar_vitoria();

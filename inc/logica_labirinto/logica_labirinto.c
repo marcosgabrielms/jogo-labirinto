e671750a8b7f3c4fa95cb371a9e018d3d02e8919
marcosgabrielms <marcosgabriel166@gmail.com>
2025-09-17 15:18:28 -0300

    :recycle: refactor(logica_labirinto): adiciona função jogo_loop()
    - Move loop principal de main para função dedicada
    - Lê MPU6050 e atualiza jogador, display e LEDs

--- inc/logica_labirinto/logica_labirinto.c
+++ inc/logica_labirinto/logica_labirinto.c
@@ -5,33 +5,15 @@
 #include "inc/logica_labirinto/logica_labirinto.h"
 #include "inc/mpu6050/mpu6050.h"
 #include <string.h>
+#include <stdio.h>
+#include <math.h> 
+#include <stdlib.h> 
 
-// --- INÍCIO DA CORREÇÃO ---
-
-// Pinos I2C corretos para o conector I2C0 da BitDogLab
+// Reuso do I2C0 da BitDogLab
 #define I2C0_SDA_PIN 0
 #define I2C0_SCL_PIN 1
 
-int main() {
-    stdio_init_all();
-    npInit(LED_PIN);
-    npClear();
-    npWrite();
-    
-    // Inicializa o display no i2c1
-    inicializacao_display(); 
-
-    // Inicializa o barramento i2c0 para o MPU6050 nos pinos corretos
-    i2c_init(i2c0, 400 * 1000);
-    gpio_set_function(I2C0_SDA_PIN, GPIO_FUNC_I2C);
-    gpio_set_function(I2C0_SCL_PIN, GPIO_FUNC_I2C);
-    gpio_pull_up(I2C0_SDA_PIN);
-    gpio_pull_up(I2C0_SCL_PIN);
-
-    // Inicializa o sensor MPU6050 no i2c0
-    mpu6050_init(i2c0);
-    inicializar_jogo();
-
+void jogo_loop() {
     int16_t accel[3], gyro[3], temp;
     char Direcao_str[20];
     char Posicao_str[20]; 
@@ -40,32 +22,47 @@ int main() {
     while (true) {
         mpu6050_read_raw(i2c0, accel, gyro, &temp);
         strcpy(Direcao_str, "Parado");
+        
+        float angle_x_rad = atan2(accel[1], accel[2]);
+        float angle_y_rad = atan2(accel[0], accel[2]);
 
-        // Diminuído o limite para aumentar a sensibilidade do movimento
-        int limite_movimento = 8000; 
+        float angle_x_deg = angle_x_rad * 180.0 / M_PI;
+        float angle_y_deg = angle_y_rad * 180.0 / M_PI;
 
-        // Lógica de movimento baseada no giroscópio
-        if (gyro[1] > limite_movimento) {
-            strcpy(Direcao_str, "Para Tras");
-            mover_jogador(0, 1);
-        } else if (gyro[1] < -limite_movimento) {
-            strcpy(Direcao_str, "Para Frente");
-            mover_jogador(0, -1);
+        float angulo_limite = 25.0;
+        float abs_angle_x = fabs(angle_x_deg);
+        float abs_angle_y = fabs(angle_y_deg);
+
+        // Bias: movimento só conta se eixo dominante for 40% maior
+        float bias = 1.4;
+
+        if (abs_angle_x > angulo_limite || abs_angle_y > angulo_limite) {
+            
+            if (abs_angle_y > abs_angle_x * bias) {
+                if (angle_y_deg > 0) {
+                    strcpy(Direcao_str, "Para Esquerda");
+                    mover_jogador(-1, 0);
+                } else {
+                    strcpy(Direcao_str, "Para Direita");
+                    mover_jogador(1, 0);
+                }
+            } 
+            else if (abs_angle_x > abs_angle_y * bias) {
+                if (angle_x_deg > 0) {
+                    strcpy(Direcao_str, "Para Tras");
+                    mover_jogador(0, -1);
+                } else {
+                    strcpy(Direcao_str, "Para Frente");
+                    mover_jogador(0, 1);
+                }
+            }
         }
 
-        if (gyro[0] > limite_movimento) {
-            strcpy(Direcao_str, "Para Direita");
-            mover_jogador(1, 0);
-        } else if (gyro[0] < -limite_movimento) {
-            strcpy(Direcao_str, "Para Esquerda");
-            mover_jogador(-1, 0);
-        }
+        printf("Angulo X: %.2f, Angulo Y: %.2f, Direcao: %s\n", angle_x_deg, angle_y_deg, Direcao_str);
 
-        // Prepara as strings para exibição no display
         sprintf(Nivel_str, "Nivel: %d", nivel);
         sprintf(Posicao_str, "Pos: (%d, %d)", jogador_x, jogador_y);
 
-        // Exibe no display as informações do jogo
         display_mensagem("JOGO LABIRINTO",
                          Nivel_str,
                          "",
@@ -78,7 +75,6 @@ int main() {
 
         desenhar_labirinto();
         verificar_vitoria();
-        sleep_ms(150);
+        sleep_ms(250);
     }
-    return 0;
 }
